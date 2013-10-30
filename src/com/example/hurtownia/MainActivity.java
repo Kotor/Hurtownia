bdd3e4e47bcd52855da6f881da76255f19e25b05
Kotor <kotor.ttc@gmail.com>
2013-10-31 00:50:15 +0100

    testowy commit

--- src/com/example/hurtownia/MainActivity.java
+++ src/com/example/hurtownia/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		RequestTask requestTask = new RequestTask();
-		
+		// TEST
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		listView = (ListView) findViewById(R.id.list);
